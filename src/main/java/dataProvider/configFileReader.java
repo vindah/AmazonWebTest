ae6ea9e49eb37bf9745946a6f2d66b9c26468ef8
vindah <patwikeyo2@gmail.com>
2022-04-14 16:51:39 +0100

    1. Added dependencies for build plugins, build profile to read config properties file. 2. Added methods to log os property and run test with chosen driver for the browser. 3. Added the driverPath in the properties file for our test. 4. Updated the configFileReader class with the methods to get the driver path as well as the application url passed earlier.

--- src/main/java/dataProvider/configFileReader.java
+++ src/main/java/dataProvider/configFileReader.java
@@ -1,4 +1,57 @@
 package dataProvider;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+
 public class configFileReader {
+    private Properties properties;
+    //private final String propertyFilePath= "src//test//resources//profiles//testProd//configuration.properties";
+    private final String propertyFilePath= getPropertyFilePath();
+
+    private String getPropertyFilePath() {
+        String profileId = System.getProperty("profileId");
+        if (profileId == null) profileId = "testProd";
+        return "src//test//resources//profiles//" + profileId + "//configuration.properties";
+    }
+
+    public configFileReader(){
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(propertyFilePath));
+            properties = new Properties();
+            try {
+                properties.load(reader);
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            throw new RuntimeException("configuration.properties not found at " + propertyFilePath);
+        }
+    }
+
+    public String getDriverPath(){
+        String driverPath = properties.getProperty("driverPath");
+        if(driverPath!= null) return driverPath;
+        else throw new RuntimeException("driverPath not specified in the configuration.properties file.");
+    }
+
+    public long getImplicitlyWait() {
+        String implicitlyWait = properties.getProperty("implicitlyWait");
+        if(implicitlyWait != null) return Long.parseLong(implicitlyWait);
+        else throw new RuntimeException("implicitlyWait not specified in the configuration.properties file.");
+    }
+
+    public String getApplicationUrl() {
+        String url = properties.getProperty("url");
+        if(url != null) return url;
+        else throw new RuntimeException("url not specified in the configuration.properties file.");
+    }
+
+
 }
